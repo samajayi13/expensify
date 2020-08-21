6bde9aa34967438304ad5e490f9c63acaf182d23
samuelisthefuckingg123 <ohajayi13@gmail.com>
2020-08-22 00:10:11 +0100

    Created Login system and redirected it to homepage if user succesfully logs in

---
+++ Scripts/loginform.js
@@ -0,0 +1,16 @@
+﻿var sumbitBtn = document.querySelector("#sumbit-btn");
+
+sumbitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    let username = document.querySelector("#username").value;
+    $.ajax({
+        url: '/Home/SetUserNameCookie',
+        data: { username: username },
+        type: "POST",
+        success: function () {
+            alert('Added');
+        }
+    });
+
+    window.location.href = '/Home/Homepage';
+});
